c8d86117a7ff947936147123519924be31fff248
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2019-11-02 16:40:58 +0100

    extended failing test for issue #10

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -20,6 +20,8 @@ describe "New author page", type: :feature do
     page.fill_in 'author[first_name]', with: 'Alan'
     page.fill_in 'author[last_name]', with: 'Turing'
     page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
-    find('input[type="submit"]').click
+    expect{
+      find('input[type="submit"]').click
+    }.to change(Author, :count).by(1)
   end
 end
