de38061dab1a0fcdc12b45d03705fea73afb5802
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2019-11-02 18:06:38 +0100

    Added failing test for #15

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "index author page", type: :feature do
+  alan = FactoryBot.create :author
+
+  it "should render withour error" do
+    visit authors_path
+  end
+
+  it "should list all authors in a table with respective links" do
+    visit authors_path
+    expect(page).to have_table
+    @table = find('table');
+    expect(@table).to have_text('Name')
+    expect(@table).to have_text('Homepage')
+
+    expect(@table).to have_text(alan.name)
+    expect(@table).to have_text(alan.homepage)
+    expect(@table).to have_link 'Show', href: author_path(alan)
+  end
+end
