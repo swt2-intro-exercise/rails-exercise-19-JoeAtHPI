4b3b44a50c96ecc914f8032c6d94ca42d4287285
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2019-11-03 00:47:55 +0100

    Added filtering for #34

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,9 @@ class Paper < ApplicationRecord
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true }
   has_and_belongs_to_many :authors
+
+  def self.created_in(year)
+    where("year = ?", year)
+  end
+
 end
